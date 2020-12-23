b93c3006d14f70ad951476e88d614f7409f487b7
kot3505 <kot35051@yandex.ru>
2020-12-24 04:09:51 +0500

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -110,6 +110,8 @@ export default (() => {
 				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
 			</Text>
 		</Section>
-
+		<Link
+			
+		</Link>
 	</Theme>;
 });
